ef2689b62b419e9df4c7f60e2b9b120527c6099b
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-04-17 15:17:32 +0100

    received 200 ok

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn = server_socket.accept()[0]
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
 
 
 if __name__ == "__main__":
