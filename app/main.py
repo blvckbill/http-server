4e019e1f90dfec8a4f019c590b9f4e4878288833
Abdulroqeeb Bakre <walebakre10@gmail.com>
2025-04-20 11:54:05 +0100

    echo string

--- app/main.py
+++ app/main.py
@@ -8,10 +8,30 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn = server_socket.accept()[0]
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn = server_socket.accept()[0] # wait for client
+    # conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    path = conn.recv(4096).decode().split(" ")[1]
+    if path == "/":
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    
+    elif path.startswith("/echo/"):
+        echo_str = path[len("/echo/"):]
+        content_length = len(echo_str.encode()) 
+        response = (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n"
+            f"Content-Length: {content_length}\r\n"
+            "\r\n"
+            f"{echo_str}"
+        )
+        conn.sendall(response.encode())
+
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 
+    
 if __name__ == "__main__":
     main()
